0887a7f65095f3755d440ff74ec7ba48d3ebb989
arodrigues7227 <arodrigues7227@gmail.com>
2024-11-22 14:45:44 -0300

    Update 20210109192536-remove-unique-constraint-to-Tickets-table.ts

--- backend/src/database/migrations/20210109192536-remove-unique-constraint-to-Tickets-table.ts
+++ backend/src/database/migrations/20210109192536-remove-unique-constraint-to-Tickets-table.ts
@@ -1,17 +1,34 @@
 import { QueryInterface } from "sequelize";
+import { QueryInterface } from "sequelize";
 
 module.exports = {
-  up: (queryInterface: QueryInterface) => {
-    return queryInterface.removeConstraint(
-      "Tickets",
-      "contactid_companyid_unique"
-    );
+  up: async (queryInterface: QueryInterface) => {
+    const [results] = await queryInterface.sequelize.query(`
+      SELECT constraint_name
+      FROM information_schema.table_constraints
+      WHERE table_name = 'Tickets' AND constraint_name = 'contactid_companyid_unique';
+    `);
+
+    if (results.length > 0) {
+      await queryInterface.removeConstraint(
+        "Tickets",
+        "contactid_companyid_unique"
+      );
+    }
   },
 
-  down: (queryInterface: QueryInterface) => {
-    return queryInterface.removeConstraint(
-      "Tickets",
-      "contactid_companyid_unique"
-    );
-  }
+  down: async (queryInterface: QueryInterface) => {
+    const [results] = await queryInterface.sequelize.query(`
+      SELECT constraint_name
+      FROM information_schema.table_constraints
+      WHERE table_name = 'Tickets' AND constraint_name = 'contactid_companyid_unique';
+    `);
+
+    if (results.length > 0) {
+      await queryInterface.removeConstraint(
+        "Tickets",
+        "contactid_companyid_unique"
+      );
+    }
+  },
 };
