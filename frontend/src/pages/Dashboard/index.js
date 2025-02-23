960c55c53f9a050d3584b96f4d80512d3e82862f
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-02-23 16:15:41 -0300

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1130,22 +1130,24 @@ const Dashboard = () => {
                       </IconButton>
 
                       <Grid2 container width="100%">
-                        {/* CARD DE GRAFICO */}
-                        {/* <Grid2 item xs={12}>
-                  <Paper
-                    elevation={6}
-                    className={classes.fixedHeightPaper}
-                  >
-                    <Chart
-                      dateStartTicket={dateStartTicket}
-                      dateEndTicket={dateEndTicket}
-                      queueTicket={queueTicket}
-                    />
-                  </Paper>
-                </Grid2>  */}
+                        <Grid2 item xs={12}>
+                          <Paper
+                            elevation={6}
+                            className={classes.fixedHeightPaper}
+                          >
+                            <Alert severity="info">
+                                Prezado atendente,<p />
+                                Esta tela exibe uma lista de usuários cadastrados na plataforma, atualizando a cada <b>1 minuto</b>. Para manter a precisão das informações exibidas, o status de conexão de cada atendente é automaticamente atualizado. <p />
+                                Se você não estiver interagindo ativamente com o sistema, seu status será alterado para <b>"Desconectado"</b> após 5 minutos. Assim que retomar a interação (por exemplo, ao clicar em menus ou enviar mensagens), seu status será atualizado para <b>"Conectado"</b>. <p />
+                                Isso permite uma visualização precisa da presença dos usuários e ajuda na gestão eficiente da plataforma. <br />
+                                Obrigado pela compreensão e aproveite o uso do sistema! <p />
+                            </Alert>
+                          </Paper>
+                      </Grid2> 
 
                         {/* INFO DOS USUARIOS */}
                         <Grid2 xs={12} id="grid-attendants">
+                          
                           {attendants.length ? (
                             <TableAttendantsStatus
                               attendants={attendants}
