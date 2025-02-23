2c2be7f158905d838ffba6e82f312ed24aba6c4b
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-02-23 16:21:33 -0300

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 // import {  Button, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from "@material-ui/core/styles";
+import Alert from '@material-ui/lab/Alert';
 import { IconButton } from "@mui/material";
 import { Groups, SaveAlt } from "@mui/icons-material";
 
